c7f6ef1a9fdf228de8ae77909eaba2a84e406280
张涵 <zhanghan30@jd.co>
2023-04-10 13:24:17 +0800

    chore(云与虚拟化): 完善生产集群部署文档

--- 云与虚拟化/调度系统/k8s/实践/k8s生产集群部署/scripts/1-update-kernel.sh
+++ 云与虚拟化/调度系统/k8s/实践/k8s生产集群部署/scripts/1-update-kernel.sh
@@ -6,4 +6,4 @@ rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 yum install https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
 yum --enablerepo=elrepo-kernel install kernel-lt
 echo "------------------------------------------------------------"
-echo "kernel updated. reboot then vim the /etc/default/grub"
+echo "kernel updated. reboot then vim the /etc/default/grub then grub2-mkconfig -o /boot/grub2/grub.cfg"
--- 云与虚拟化/调度系统/k8s/实践/k8s生产集群部署/scripts/5-turn-off-firewall.sh
+++ 云与虚拟化/调度系统/k8s/实践/k8s生产集群部署/scripts/5-turn-off-firewall.sh
@@ -1,4 +1,5 @@
 set -e
 echo "turning firewalld off"
+echo "------------------------------------------------------------"
 systemctl stop firewalld
 systemctl disable firewalld
